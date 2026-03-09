df2448affe25ad3c4f9d65aa1e45edd441906bff
pdmoura <sudwarriormoura@gmail.com>
2026-03-08 23:34:16 -0300

    feat: implement mongodb connection logic with mongoose

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,12 @@
-export const connectDB = async () => {}; // TODO: Implementar a conexão com o banco de dados
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    
+    console.log(`Log: MongoDB Conectado: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Erro ao conectar ao MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
