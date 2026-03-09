07283f153cc404a1cc50c1dae17ac922b9b7d3cd
pdmoura <sudwarriormoura@gmail.com>
2026-03-09 03:15:04 -0300

    refactor: simplify vite config and remove unused env example

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,33 +1,23 @@
-/* eslint-disable no-undef */
-
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-
+/* eslint-env node */
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [
-      react({
-        babel: {
-          plugins: [['babel-plugin-react-compiler']],
-
-        },
-      }),
-      tailwindcss(),
-    ],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL || 'http://localhost:5000',
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
       },
-    },
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', 
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
