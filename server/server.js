b436a84a919710965922d2274675643af4f2aa33
pdmoura <sudwarriormoura@gmail.com>
2026-03-09 00:00:03 -0300

    feat: adiciona estrutura inicial do backend server

--- server/server.js
+++ server/server.js
@@ -11,8 +11,7 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); 
-
+app.use(express.json());
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({ 
