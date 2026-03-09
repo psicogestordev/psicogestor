b89252f07dc4fd441fae8a1b43769c75a66f9bd4
pdmoura <sudwarriormoura@gmail.com>
2026-03-08 23:06:11 -0300

    refactor: remove arquivos .env do client e corrige configuração do Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,23 @@
-/* eslint-disable no-undef */
-
-import { defineConfig, loadEnv } from 'vite'
+/* eslint-env node */
+// https://vite.dev/config/
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [
-      react({
-        babel: {
-          plugins: [['babel-plugin-react-compiler']],
-        },
-      }),
-    ],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL || 'http://localhost:5000',
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
       },
-    },
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', 
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
