5ed01497cef7aeeb1a32aef565ab7e40ecebdaf6
lunah <lunah.silvac@gmail.com>
2026-03-16 22:47:02 -0500

    feat: implementado models que o sistema vai usar

--- server/server.js
+++ server/server.js
@@ -4,24 +4,27 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
 
-dotenv.config();
-connectDB();
+const start = async () => {
+  await connectDB();
 
-const app = express();
+  const app = express();
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
+  // Middlewares
+  app.use(cors());
+  app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-  res.status(200).json({ 
-    status: "ok", 
-    message: "Servidor rodando perfeitamente!" 
+  app.get('/api/health', (req, res) => {
+    res.status(200).json({
+      status: 'ok',
+      message: 'Servidor rodando perfeitamente!',
+    });
   });
-});
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Log: Servidor rodando na porta ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Log: Servidor rodando na porta ${PORT}`);
+  });
+};
+
+start();
