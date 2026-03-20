3297e4d1203a3e89d23cf374b824afd26aedec9c
pdmoura <sudwarriormoura@gmail.com>
2026-03-20 14:43:04 -0300

    fix: change authentication routes to use /api/auth

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const start = async () => {
   app.use(express.json());
 
   // routes
-  app.use('/', authRoutes);
+  app.use('/api/auth', authRoutes);
 
   app.get('/api/health', (req, res) => {
     res.status(200).json({
